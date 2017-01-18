bab7798641183c76dd6a37452c5d0b148d5d564d
Oluwatomi <t_aquarius99@yahoo.com>
2017-01-18 13:12:17 +0100

    Added function to view all contacts

---
+++ viewcontacts.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+var firebase = require("firebase");
+
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyBvo5Y-wKqsIqJS2xKC3HGyjSIO--krl0U",
+	authDomain: "contact-manager-71833.firebaseapp.com",
+	databaseURL: "https://contact-manager-71833.firebaseio.com",
+	storageBucket: "contact-manager-71833.appspot.com",
+	messagingSenderId: "867603135513"
+};
+firebase.initializeApp(config);
+
+var ref = firebase.database().ref();
+var contactRef = ref.child('contact');
+
+function viewAllContacts(){
+
+	var names = [];
+	var phone_no = [];
+
+	if(process.argv[2] === "-a" && process.argv.length === 3){
+		
+		contactRef.on('value', function(snapshot){
+			var contacts = snapshot.val();
+			var keys = Object.keys(contacts);
+			var count = 0;
+			var answer;
+
+			for (var i = 0; i < keys.length; i++) {
+				var k = keys[i];
+				names.push(contacts[k].name);
+				phone_no.push(contacts[k].phone_no);
+				count++;
+				console.log("[" + count + "] " + names[i]);
+			}
+			console.log('\n' + count + " contacts found!")
+			console.log("Enter the associated number to view the phone number or type anything else to exit\n");
+			process.stdin.on('data', function(data){
+				response = parseInt(data)
+				for (var i = 1; i <= count; i++) {
+					if(response === i){
+						process.stdout.write("\n" + phone_no[i-1] + "\n");
+						break;
+					}
+				}
+				process.exit();
+			});
+		});
+
+	}else{
+		console.log("Oops! something went wrong with your command!");
+		process.exit();
+	}
+}
+
+viewAllContacts();
