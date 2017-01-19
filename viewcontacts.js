54efaff6429a4920fa4c689b071cc666d9ee1421
Oluwatomi <t_aquarius99@yahoo.com>
2017-01-19 13:24:35 +0100

    Modified file not to expose keys

--- viewcontacts.js
+++ viewcontacts.js
@@ -1,14 +1,9 @@
 #!/usr/bin/env node
+
 var firebase = require("firebase");
 
 // Initialize Firebase
-var config = {
-	apiKey: "AIzaSyBvo5Y-wKqsIqJS2xKC3HGyjSIO--krl0U",
-	authDomain: "contact-manager-71833.firebaseapp.com",
-	databaseURL: "https://contact-manager-71833.firebaseio.com",
-	storageBucket: "contact-manager-71833.appspot.com",
-	messagingSenderId: "867603135513"
-};
+var config = require("./config/config-firebase.js");
 firebase.initializeApp(config);
 
 var ref = firebase.database().ref();
