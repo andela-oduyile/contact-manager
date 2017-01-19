bfa62c5148505c1f4d2f59ac59f61512bec21c3c
Oluwatomi <t_aquarius99@yahoo.com>
2017-01-19 17:45:24 +0100

    Added a processing message when loading

--- index.js
+++ index.js
@@ -3,13 +3,7 @@
 var firebase = require("firebase");
 
 // Initialize Firebase
-var config = {
-	apiKey: "AIzaSyBvo5Y-wKqsIqJS2xKC3HGyjSIO--krl0U",
-	authDomain: "contact-manager-71833.firebaseapp.com",
-	databaseURL: "https://contact-manager-71833.firebaseio.com",
-	storageBucket: "contact-manager-71833.appspot.com",
-	messagingSenderId: "867603135513"
-};
+var config = require("./config/config-firebase.js");
 firebase.initializeApp(config);
 
 var ref = firebase.database().ref();
@@ -25,6 +19,8 @@ function get(flag) {
 	}
 }
 
+console.log("Processing...");
+
 function addContact() {
 
 	var name = get('-n');
@@ -36,6 +32,14 @@ function addContact() {
 		contactRef.push({
 			'name': name,
 			'phone_no': phone_no
+
+		}, function(err){
+			if(!err){
+				console.log("Your contact has been added");
+				process.exit();
+			}else{
+				console.log("An error occured!");
+			}
 		});
 	}
 
