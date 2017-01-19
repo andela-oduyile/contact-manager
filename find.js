d936e18ae9db7b465424b9c848085f6a2a17aa53
Oluwatomi <t_aquarius99@yahoo.com>
2017-01-19 09:15:12 +0100

    Modified search function to use firebase search feature

--- find.js
+++ find.js
@@ -1,15 +1,10 @@
 #!/usr/bin/env node
 
 var firebase = require("firebase");
+var async = require('async');
 
 // Initialize Firebase
-var config = {
-	apiKey: "AIzaSyBvo5Y-wKqsIqJS2xKC3HGyjSIO--krl0U",
-	authDomain: "contact-manager-71833.firebaseapp.com",
-	databaseURL: "https://contact-manager-71833.firebaseio.com",
-	storageBucket: "contact-manager-71833.appspot.com",
-	messagingSenderId: "867603135513"
-};
+var config = require("./config/config-firebase.js");
 firebase.initializeApp(config);
 
 var ref = firebase.database().ref();
@@ -17,22 +12,24 @@ var contactRef = ref.child('contact');
 
 var names = [];
 var phone_no = [];
+var searchItem;
 
 
-
-function search(searchItem){
-
-	if(!process.argv[2]){
-		console.log("Oops! something went wrong");
-		process.exit();
-	}else{
+function search(){
+	if(process.argv[2]){
 		searchItem = process.argv[2];
 	}
 
-	contactRef.on('value', function(snapshot){
-		var contacts = snapshot.val();
-		var keys = Object.keys(contacts);
-		var first_names = [];
+	var names = [];
+	var phone_no = [];
+
+	contactRef.orderByChild('name')
+    .startAt(searchItem)
+    .endAt(searchItem + '\uf8ff')
+    .on('value', function(snap) {
+    	var contacts = snap.val();
+    	var keys = Object.keys(contacts);
+    	var first_names = [];
 		var last_names = [];
 		var duplicates = {
 			"names" : [],
@@ -42,7 +39,6 @@ function search(searchItem){
 		var prompt;
 		var count = 0;
 		var answer;
-		
 
 		for (var i = 0; i < keys.length; i++) {
 			var k = keys[i];
@@ -58,12 +54,11 @@ function search(searchItem){
 		for (var i = 0; i < names.length; i++) {
 			if(names[i] === searchItem){
 				console.log(phone_no[i]);
-				found = true;
 				process.exit();
 			}
 			
 			if(first_names[i] === searchItem || last_names[i] === searchItem){
-				duplicates.names.push(first_names[i]);
+				duplicates.names.push(last_names[i]);
 				duplicates.phone_no.push(phone_no[i]);
 				found = true;
 			}
@@ -87,25 +82,24 @@ function search(searchItem){
 			process.stdin.on('data', function(data){
 				answer = parseInt(data);
 				
-				if(answer === 1){
-					process.stdout.write(duplicates.phone_no[0]);
-					process.exit();
-				}else if(answer === 2){
-					process.stdout.write(duplicates.phone_no[1]);
-					process.exit();
-				}else if(answer === 3){
-					process.stdout.write(duplicates.phone_no[2]);
-					process.exit();
-				}else{
-					process.stdout.write("Incorrect Option");
-					process.exit();
+				for (var i = 0; i < duplicates.names.length; i++) {
+					if(answer === i+1){
+						process.stdout.write(duplicates.phone_no[i]);
+						process.exit();
+					}
 				}
+				
+				process.stdout.write("Incorrect Option");
+				process.exit();
+				
 			});
 		}
 
-		
-	});
+    });
+    
 
 }
 
-search(process.argv[2]);
+module.exports.search = search;
+
+search();
